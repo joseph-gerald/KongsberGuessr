6f64013d8ee3f2c29613f293d57c0b46a06297f3
joseph-gerald <73967013+joseph-gerald@users.noreply.github.com>
2023-11-21 22:52:18 +0100

    Created Auth Endpoints

---
+++ website/src/pages/api/auth/register.ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function register(req: NextApiRequest, res: NextApiResponse) {
+    if (typeof req.body !== 'object' || req.headers['content-type'] != "application/json") {
+        res.status(400).json({ error: 'Expected a JSON body' })
+        return
+    }
+
+    if(["username","password"].some((e) => !req.body[e])) {
+        res.status(400).json({ error: 'Missing required fields' })
+        return
+    }
+
+    // TODO: Implement registration logic tmrw
+}
