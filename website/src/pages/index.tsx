fc2f5658e353c4a7c6d263191708c172c46d49fa
joseph-gerald <73967013+joseph-gerald@users.noreply.github.com>
2023-11-22 23:38:30 +0100

    Pagination Dots buttoninization

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -4,12 +4,12 @@ import Features from "./sections/features";
 import { useEffect } from "react";
 
 export default function Index() {
+    let sectionIndex = 0;
 
     const syncScroll = () => {
         const sections = document.querySelectorAll('.section');
         const dots = document.querySelectorAll('.pagination-dot');
         let canScroll = true;
-        let sectionIndex = 0;
 
         function doScroll() {
             if(screen.width < 1024) return;
@@ -63,7 +63,7 @@ export default function Index() {
             </div>
             <div className="pagination hidden lg:flex">
                 {[0, 1, 2].map((i) => (
-                    <div key={i} className={"pagination-dot"}></div>
+                    <div key={i} onClick={() => sectionIndex = i} className="pagination-dot cursor-pointer"></div>
                 ))}
             </div>
         </>
